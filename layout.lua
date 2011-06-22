0a9a3c81a46d2fcff1cb9ab37e3d9db637b5600d
unknown <spamdead.mb@gmail.com>
2011-06-23 00:11:13 +0200

    Further preparation

--- layout.lua
+++ layout.lua
@@ -11,4 +11,12 @@ local core = ns.core
 
 -- Let's get it on!
 local Splash = CreateFrame('Frame')
+SplashSettings = nil
 -- *********************************************************************************
+
+Splash:RegisterEvent('ADDON_LOADED')
+Splash:SetScript('OnEvent', function(self, event, addon)
+	if ( addon ~= ADDON_NAME ) then return end
+
+	SplashSettings = settings.init
+end)
--- settings.lua
+++ settings.lua
@@ -7,5 +7,34 @@ local ADDON_NAME, ns = ...
 local settings = {}
 -- *********************************************************************************
 
+settings.fonts = {
+	['grunge'] = [[Interface\AddOns\oUF_Splash\media\Coalition.ttf]],
+	['clear'] = [[Interface\AddOns\oUF_Splash\media\accid__.ttf]],
+}
+
+settings.tex = {
+	['hp'] = [[Interface\AddOns\oUF_Splash\media\hp_fg.tga]],
+	['hp_bg'] = [[Interface\AddOns\oUF_Splash\media\hp_bg.tga]],
+	['power'] = [[Interface\AddOns\oUF_Splash\media\power_fg.tga]],
+	['solid'] = [[Interface\AddOns\oUF_Splash\media\solid.tga]],
+	['gloss'] = [[Interface\AddOns\oUF_Splash\media\gloss.tga]],
+	['gloss_aura'] = [[Interface\AddOns\oUF_Splash\media\gloss_aura.tga]],
+	['overlay'] = [[Interface\AddOns\oUF_Splash\media\overlay.tga]],
+}
+
+settings.init = {
+	['general'] = {
+		['barColor'] = { 0.4, 0.4, 0.4 },
+		['barBGColor'] = { 0.7, 0.7, 0.7, 1 },
+		['powerBGModifier'] = 0.3,
+	},
+	['player'] = {
+		['width'] = 180,
+		['height'] = 32,
+		['powerHeight'] = 5,
+		['powerXOff'] = 10,
+	}
+}
+
 -- *********************************************************************************
 ns.settings = settings
