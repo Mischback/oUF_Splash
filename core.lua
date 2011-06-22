9053f18ba50b917d83c369b9e65adb6f6bec5c71
unknown <spamdead.mb@gmail.com>
2011-06-23 00:45:32 +0200

    First UF

--- core.lua
+++ core.lua
@@ -12,5 +12,35 @@ local lib = ns.lib
 local core = {}
 -- *********************************************************************************
 
+core.CreateUnitFrame = function(self, key)
+
+	cfg = SplashSettings[key]
+
+	self:SetSize(cfg.width, cfg.height)
+
+	-- Creating the Health-bar
+	local hp = CreateFrame('StatusBar', nil, self)
+	hp:SetFrameLevel(11)
+	hp:SetAllPoints(self)
+	hp:SetStatusBarTexture(settings.tex.hp)
+	hp:SetStatusBarColor(unpack(SplashSettings.general.barColor))
+
+	hp.bg = hp:CreateTexture(nil, 'BACKGROUND')
+	hp.bg:SetAllPoints(hp)
+	hp.bg:SetTexture(settings.tex.hp_bg)
+	hp.bg:SetVertexColor(unpack(SplashSettings.general.barBGColor))
+
+	hp.border = CreateFrame('Frame', nil, hp)
+	hp.border:SetFrameLevel(10)
+	hp.border:SetPoint('TOPLEFT', hp, 'TOPLEFT', -1, 1)
+	hp.border:SetPoint('BOTTOMRIGHT', hp, 'BOTTOMRIGHT', 1, -1)
+	hp.border.tex = hp.border:CreateTexture(nil, 'BACKGROUND')
+	hp.border.tex:SetAllPoints(hp.border)
+	hp.border.tex:SetTexture(0, 0, 0, 1)
+
+	self.Health = hp
+
+end
+
 -- *********************************************************************************
 ns.core = core
--- layout.lua
+++ layout.lua
@@ -14,9 +14,18 @@ local Splash = CreateFrame('Frame')
 SplashSettings = nil
 -- *********************************************************************************
 
+local function createPlayer(self)
+	core.CreateUnitFrame(self, 'player')
+end
+
+-- *********************************************************************************
 Splash:RegisterEvent('ADDON_LOADED')
 Splash:SetScript('OnEvent', function(self, event, addon)
 	if ( addon ~= ADDON_NAME ) then return end
 
 	SplashSettings = settings.init
+
+	oUF:RegisterStyle('oUF_Splash_player', createPlayer)
+
+	oUF:Spawn('player', 'oUF_Splash_player'):SetPoint('RIGHT', UIParent, 'CENTER', -100, -200)
 end)
